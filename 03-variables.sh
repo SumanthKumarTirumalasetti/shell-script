ff0f4a6d6b9316d7d70423e3aaeae52588bb95b0
Tirumalasetti <st185178@ncr.com>
2025-01-05 16:21:12 +0530

    Updated 2 and 3

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=Emu
+Person2=Laddu
+
+echo "$person1:: Hi $person2"
+echo "$person2:: Hi $person1"
+echo "$person1:: what are you doing $person2"
+echo "$person2:: waiting for you $person1"
