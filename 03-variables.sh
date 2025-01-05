9f0ab187dd2cf1fde7a6fba7b6a286f04ec3fb14
Tirumalasetti <st185178@ncr.com>
2025-01-05 16:28:32 +0530

    updating

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,6 +4,6 @@ PERSON1=Sumanth
 PERSON2=Saritha
 
 echo "$PERSON1:: Hi $PERSON2"
-echo "$PERSON2:: Hi $PERSON1"
+echo "$(PERSON2):: Hi $PERSON1"
 echo "$PERSON1:: what are you doing $PERSON2"
 echo "$PERSON2:: waiting for you $PERSON1"
