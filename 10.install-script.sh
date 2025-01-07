ec03d9a8aa174be2920f67ee350588ca8acf1efe
Tirumalasetti <st185178@ncr.com>
2025-01-07 21:59:41 +0530

    Updated

--- 10.install-script.sh
+++ 10.install-script.sh
@@ -8,10 +8,10 @@ if [ $CHECKUSERID -eq 0 ]
 then
     echo "Current user is root user and the id is : $CHECKUSERID"
     echo "Installing MYSQL Server"
-    dnf install mysql-server -y > log.txt
+    dnf install mysql-server -y >> log.txt
 else
     echo "Switching user to root and installing MySql Server"
-    sudo dnf install mysql-server -y > log.txt
+    sudo dnf install mysql-server -y >> log.txt
 fi
 
 if [ $? -eq 0 ]
@@ -22,10 +22,10 @@ else
 fi
 
 echo "enabling the mysql-server"
-sudo systemctl enable mysqld > log.txt
+sudo systemctl enable mysqld >> log.txt
 
 echo "Start the mysql-server"
-sudo systemctl start mysqld > log.txt
+sudo systemctl start mysqld >> log.txt
 
 echo "Checking the SQL Service status"
-sudo systemctl status mysqld > log.txt
+sudo systemctl status mysqld >> log.txt
