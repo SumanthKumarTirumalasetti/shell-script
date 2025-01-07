ad8d17706b4152575c8207361795964900353aa5
Tirumalasetti <st185178@ncr.com>
2025-01-07 21:41:11 +0530

    Updated

--- 10.install-script.sh
+++ 10.install-script.sh
@@ -8,10 +8,10 @@ if [ $CHECKUSERID -eq 0 ]
 then
     echo "Current user is root user and the id is : $CHECKUSERID"
     echo "Installing MYSQL Server"
-    dnf install mysql-server -y
+    dnf install mysql-server -y > log.txt
 else
     echo "Switching user to root and installing MySql Server"
-    sudo dnf install mysql-server -y
+    sudo dnf install mysql-server -y > log.txt
 fi
 
 #dnf install mysql-server -y
