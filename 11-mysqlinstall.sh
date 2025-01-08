a7f8edeef0f10e9a8914e42734779da84278a9b6
Tirumalasetti <st185178@ncr.com>
2025-01-08 21:22:42 +0530

    updated

--- 11-mysqlinstall.sh
+++ 11-mysqlinstall.sh
@@ -5,9 +5,36 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "You are not a admin user and you must have admin access to run the program"
-    
+    exit 1
 fi
 
-echo $?
-
 echo "Installing mysql-server"
+dnf install mysql-server -y
+
+if [ $? -eq 0 ]
+then
+    echo "MySql server installation completed successfully"
+else
+    echo "MySql server installation failed"
+fi
+
+echo "Starting mysql-server"
+systemctl start mysqld
+
+if [ $? -eq 0 ]
+then
+    echo "MySql service has been started successfully"
+else
+    echo "MySql service has not been started successfully"
+fi
+
+echo "enabling mysql-server"
+systemctl enable mysqld
+
+if [ $? -eq 0 ]
+then
+    echo "MySql server has been enabled successfully"
+else
+    echo "MySql server has not been enabled"
+fi
+
