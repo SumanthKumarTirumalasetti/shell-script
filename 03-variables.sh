84e054f769a7009be465389a9062a9cc591804aa
Sumanth Tirumalasetti <st185178@candescent.com>
2025-08-18 20:41:11 +0530

    Publishing 03-variables.sh

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 PERSON1=Sumanth
-PERSON2=Saritha
+PERSON2=Mounika
 
 echo "$PERSON1:: Hi $PERSON2"
-echo "${PERSON2}:: Hi $PERSON1"
-echo "$PERSON1:: what are you doing $PERSON2"
-echo "$PERSON2:: waiting for you $PERSON1"
+echo "${PERSON2}:: Hello $PERSON1"
+echo "Sumanth:: How are you doing?"
+echo "$PERSON2:: I am good. How are you?"
--- 04-variables.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PERSON1=$1
-PERSON2=$2
-
-echo "$PERSON1:: Hi $PERSON2"
-echo "${PERSON2}:: Hi $PERSON1"
-echo "$PERSON1:: what are you doing $PERSON2"
-echo "$PERSON2:: waiting for you $PERSON1"
