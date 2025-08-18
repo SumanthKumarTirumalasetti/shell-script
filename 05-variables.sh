66f6086c4a2510b9ba2ebfe89728b6b478e5a20f
Sumanth Tirumalasetti <st185178@candescent.com>
2025-08-18 20:48:04 +0530

    Taking user input from argument level

---
+++ 05-variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Taking user input from argument level from command line
+
+echo "Please enter PERSON1 USERNAME:"
+read -s PRESON1
+
+echo "Please enter PERSON2 USERNAME:"
+read -s PERSON2
+
+echo "$PERSON1:: Hi $PERSON2"
+echo "${PERSON2}:: Hello $PERSON1"
+echo "Sumanth:: How are you doing?"
+echo "$PERSON2:: I am good. How are you?"
