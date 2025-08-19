db90defaf9d4665720452ce804c4b62f04bee326
Sumanth Tirumalasetti <st185178@candescent.com>
2025-08-19 11:06:25 +0530

    Taking user input using read -s command

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# Arrays
+
 MOVIES=("Pushpa" "rrr" "devara")
 
 echo "First movie: ${MOVIES[0]}"
 echo "Second movie: $MOVIES[1]"
 echo "Third movie: $MOVIES[2]"
 
-echo "All movies are: $MOVIES[@]"
+echo "All movies are:" $MOVIES[@]
