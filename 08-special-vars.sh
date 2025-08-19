55eeff6727bd1ccf8352db86fe6835eee78e7e6c
Sumanth Tirumalasetti <st185178@candescent.com>
2025-08-19 11:09:50 +0530

    Taking user input using read -s command

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -9,5 +9,5 @@ echo "Present working directory : $PWD"
 echo "User home directory : $HOME"
 echo "which user is running this script : $USER"
 echo "Process of current script : $$"
-sleep 60 
+sleep 60 &
 echo "Process id of last command in background : $!"
