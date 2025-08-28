6d44ea4ae945ac6c5c9cc1cec4d9e79e0474cd3f
Sumanth Tirumalasetti <st185178@candescent.com>
2025-08-28 20:36:35 +0530

    Using colours

--- 12-colors.sh
+++ 12-colors.sh
@@ -18,6 +18,11 @@ validate() {
     fi
 }
 
+if [ $USERID -ne 0 ]
+then
+    echo  "ERROR:: You must have sudo access to execute this script"
+    exit 1 # otherthan 0
+fi
 
 dnf list installed mysql
 
