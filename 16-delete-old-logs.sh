2cf7f2c42bf7b4de480f9d349452bb76e9252053
Sumanth Tirumalasetti <st185178@candescent.com>
2025-09-04 05:40:59 +0530

    deleting old logs

---
+++ 16-delete-old-logs.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIME_STAMP.log"
+
+
+validate() {
+
+    if [ $? -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+remove() {
+
+    if [ $? -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+CHECK_ROOT() {
+
+    if [ $USERID -ne 0 ]
+    then
+        echo "ERROR:: You must have sudo access to execute this script"
+        exit 1
+    fi
+}
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+FILES_TO_DELETE=$(find $LOGS_FOLDER -name "*.log" -mtime +14)
+echo "Files to be deleted: $FILES_TO_DELETE"
